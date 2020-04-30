d554c8049b4d8797894ea46f31f1f7617565c22d
amenpunk <ondasycircuitos@gmail.com>
2020-04-29 22:50:45 -0600

    [read] read all the server apis

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,3 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express')
@@ -9,17 +8,18 @@ admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
 
-app.post('/getStreamUrl', async(req,res) => {
+app.post('/SetStream', async(req,res) => {
     try{
+        const {URL, Name} = req.body;
 
-        const snap = await db.collection("Usuarios").get()
-        const data = snap.docs.map(ele => ele.data())
-
-        if(data.length >=1){
-            return res.send(data)
-        }else{
-            return res.send({"Error" : "No pudimos se pudo localizar el server"})
+        if(!URL || !Name){
+            return res.send({"Error" : "Por favor ingresa un URL o nombre valido"})
         }
+
+        await db.collection("Platforms").doc(Mail).update({
+            [name]  : URL
+        })
+        return res.send({"Success" : "Tu nuevo stream ha sido agregado!!!"})
     }catch(err){
         console.log(err)
         return res.send({"Error" : "No pudimos se pudo localizar el server"})
@@ -31,10 +31,20 @@ app.post('/GetStream', async (req,res) => {
         console.log("request start")
         const {Mail} = req.body;
         console.log(Mail)
-        const snap = await db.collection("Platforms").doc(Mail).get()  
-        const url = snap.data();
-        if(snap.exists){
-            return res.send(url)
+        const snap = await db.collection("Platforms").doc(Mail).collection("List").get();
+        const platList = snap.docs.map(ele => ele.data())
+        if(platList.length >= 1 ){
+            const plataformas = platList.map(plat => {
+                let tem = {
+                    app : plat.Name,
+                    mode : "push",
+                    edge : plat.URL,
+                    appendName : false
+                }
+                return tem
+            })
+
+            return res.send(plataformas)
         }else{
             return res.send({"Error" : "No pudimos se pudo localizar el server"})
         }
