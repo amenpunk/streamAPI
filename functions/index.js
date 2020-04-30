c6c3418b6f1f533eb27e292b395cd2d1214b1a8d
amenpunk <ondasycircuitos@gmail.com>
2020-04-29 21:02:29 -0600

    [endpoints]

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,51 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
-admin.initializeApp();
-const {app} = require('./routes.js')
+const express = require('express')
+const app = express();
+const cors = require('cors'); 
+app.use(cors({ origin: true  })); 
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
+app.post('/getStreamUrl', async(req,res) => {
+    try{
+
+        const snap = await db.collection("Usuarios").get()
+        const data = snap.docs.map(ele => ele.data())
+
+        if(data.length >=1){
+            return res.send(data)
+        }else{
+            return res.send({"Error" : "No pudimos se pudo localizar el server"})
+        }
+    }catch(err){
+        console.log(err)
+        return res.send({"Error" : "No pudimos se pudo localizar el server"})
+    }
+})
+
+app.post('/GetStream', async (req,res) => {
+    try{
+        console.log("request start")
+        const {Mail} = req.body;
+        console.log(Mail)
+        const snap = await db.collection("Platforms").doc(Mail).get()  
+        const url = snap.data();
+        if(snap.exists){
+            return res.send(url)
+        }else{
+            return res.send({"Error" : "No pudimos se pudo localizar el server"})
+        }
+    }catch(err){
+        console.log(err)
+        return res.send({"Error" : "No pudimos se pudo localizar el server"})
+    }
+})
+
+
+//module.exports.router = router;
+
 exports.API = functions.https.onRequest(app);
 
