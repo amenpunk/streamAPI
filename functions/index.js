92c084e5193a4dab96ece508bb421c82d9508bec
amenpunk <ondasycircuitos@gmail.com>
2020-05-01 15:49:55 -0600

    [ADD] delete stream endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,61 +1,115 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const express = require('express')
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
 const app = express();
-const cors = require('cors'); 
-app.use(cors({ origin: true  })); 
+const cors = require("cors");
+app.use(cors({ origin: true }));
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
+app.post("/SetStream", async (req, res) => {
+    try {
+        const { URL, Name } = req.body;
 
-app.post('/SetStream', async(req,res) => {
-    try{
-        const {URL, Name} = req.body;
-
-        if(!URL || !Name){
-            return res.send({"Error" : "Por favor ingresa un URL o nombre valido"})
+        if (!URL || !Name) {
+            return res.send({
+                Error: "Por favor ingresa un URL o nombre valido"
+            });
         }
 
-        await db.collection("Platforms").doc(Mail).update({
-            [name]  : URL
-        })
-        return res.send({"Success" : "Tu nuevo stream ha sido agregado!!!"})
-    }catch(err){
-        console.log(err)
-        return res.send({"Error" : "No pudimos se pudo localizar el server"})
+        await db
+            .collection("Platforms")
+            .doc(Mail)
+            .update({
+                [name]: URL
+            });
+        return res.send({ Success: "Tu nuevo stream ha sido agregado!!!" });
+    } catch (err) {
+        console.log(err);
+        return res.send({ Error: "No pudimos se pudo localizar el server" });
     }
-})
+});
 
-app.post('/GetStream', async (req,res) => {
-    try{
-        console.log("request start")
-        const {Mail} = req.body;
-        console.log(Mail)
-        const snap = await db.collection("Platforms").doc(Mail).collection("List").get();
-        const platList = snap.docs.map(ele => ele.data())
-        if(platList.length >= 1 ){
+app.post("/GetStream", async (req, res) => {
+    try {
+        console.log("request start");
+        console.log(req.body);
+        const { Mail, apikey } = req.body;
+
+        if (apikey !== Mail) {
+            return res
+                .status(401)
+                .send({ Error: "No estas autorizado para ver el contenido" });
+        }
+        const snap = await db
+            .collection("Platforms")
+            .doc(Mail)
+            .collection("List")
+            .get();
+        const platList = snap.docs.map(ele => ele.data());
+        if (platList.length >= 1) {
             const plataformas = platList.map(plat => {
                 let tem = {
-                    app : plat.Name,
-                    mode : "push",
-                    edge : plat.URL,
-                    appendName : false
-                }
-                return tem
-            })
+                    app: plat.Name,
+                    mode: "push",
+                    edge: plat.URL,
+                    appendName: false
+                };
+                return tem;
+            });
 
-            return res.send(plataformas)
-        }else{
-            return res.send({"Error" : "No pudimos se pudo localizar el server"})
+            return res.send(plataformas);
+        } else {
+            let tem =[{
+                app: "",
+                mode: "push",
+                edge: "No tienes ninguna plataforma configurada",
+                appendName: false
+            }];
+            return res.send(tem);
         }
-    }catch(err){
-        console.log(err)
-        return res.send({"Error" : "No pudimos se pudo localizar el server"})
+    } catch (err) {
+        console.log(err);
+        let tem = [{
+            app: "",
+            mode: "push",
+            edge: "No tienes ninguna plataforma configurada",
+            appendName: false
+        }];
+        return res.send(tem);
     }
+});
+
+app.post("/DeleteStream", async (req,res) => {
+    const {Mail, apikey, URL} = req.body;
+    console.log(req.body)
+    if (apikey !== Mail) {
+        return res
+            .status(401)
+            .send({ Error: "No estas autorizado para ver el contenido" });
+    }
+    const snap = await db.collection("Platforms").doc(Mail).collection("List").where("URL", "==", URL).get();
+    const listSnap = snap.docs.map(ele => {
+        let temp = { ID : ele.id }
+        Object.assign(temp, ele.data())
+        return temp
+    });
+
+    if(listSnap.length >= 1){
+        const {ID} = listSnap.shift();
+        await db.collection("Platforms").doc(Mail).collection("List").doc(ID).delete()
+        return res.send({
+            status : 1,
+            message : "Stream eliminado"
+        })
+    }else{
+        return res.send({
+            status : 0,
+            message : "No se pudo eliminar el stream"
+        })
+
+    }
+
 })
 
-
-//module.exports.router = router;
-
 exports.API = functions.https.onRequest(app);
-
