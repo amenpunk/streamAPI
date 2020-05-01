23762f2cfbff919eae094f93a3149d6e0c807348
amenpunk <ondasycircuitos@gmail.com>
2020-05-01 16:07:11 -0600

    --amend

--- functions/index.js
+++ functions/index.js
@@ -60,56 +60,125 @@ app.post("/GetStream", async (req, res) => {
 
             return res.send(plataformas);
         } else {
-            let tem =[{
-                app: "",
-                mode: "push",
-                edge: "No tienes ninguna plataforma configurada",
-                appendName: false
-            }];
+            let tem = [
+                {
+                    app: "",
+                    mode: "push",
+                    edge: "No tienes ninguna plataforma configurada",
+                    appendName: false
+                }
+            ];
             return res.send(tem);
         }
     } catch (err) {
         console.log(err);
-        let tem = [{
-            app: "",
-            mode: "push",
-            edge: "No tienes ninguna plataforma configurada",
-            appendName: false
-        }];
+        let tem = [
+            {
+                app: "",
+                mode: "push",
+                edge: "No tienes ninguna plataforma configurada",
+                appendName: false
+            }
+        ];
         return res.send(tem);
     }
 });
 
-app.post("/DeleteStream", async (req,res) => {
-    const {Mail, apikey, URL} = req.body;
-    console.log(req.body)
+app.post("/DeleteStream", async (req, res) => {
+    const { Mail, apikey, URL } = req.body;
+    console.log(req.body);
+
     if (apikey !== Mail) {
         return res
             .status(401)
             .send({ Error: "No estas autorizado para ver el contenido" });
     }
-    const snap = await db.collection("Platforms").doc(Mail).collection("List").where("URL", "==", URL).get();
-    const listSnap = snap.docs.map(ele => {
-        let temp = { ID : ele.id }
-        Object.assign(temp, ele.data())
-        return temp
-    });
+    try {
+        const snap = await db
+            .collection("Platforms")
+            .doc(Mail)
+            .collection("List")
+            .where("URL", "==", URL)
+            .get();
+        const listSnap = snap.docs.map(ele => {
+            let temp = { ID: ele.id };
+            Object.assign(temp, ele.data());
+            return temp;
+        });
 
-    if(listSnap.length >= 1){
-        const {ID} = listSnap.shift();
-        await db.collection("Platforms").doc(Mail).collection("List").doc(ID).delete()
+        if (listSnap.length >= 1) {
+            const { ID } = listSnap.shift();
+            await db
+                .collection("Platforms")
+                .doc(Mail)
+                .collection("List")
+                .doc(ID)
+                .delete();
+            return res.send({
+                status: 1,
+                message: "Stream eliminado"
+            });
+        } else {
+            return res.send({
+                status: 0,
+                message: "Stream no pudo ser  eliminado"
+            });
+        }
+    } catch (err) {
         return res.send({
-            status : 1,
-            message : "Stream eliminado"
-        })
-    }else{
-        return res.send({
-            status : 0,
-            message : "No se pudo eliminar el stream"
-        })
-
+            status: 0,
+            message: "Stream no puso ser eliminaod",
+            reason: err.message
+        });
     }
+});
 
-})
+app.post("/AddStream", async (req, res) => {
+    const { Mail, apikey, URL } = req.body;
+    console.log(req.body);
+
+    try {
+        if (apikey !== Mail) {
+            return res
+                .status(401)
+                .send({ Error: "No estas autorizado para ver el contenido" });
+        }
+        const add = await db
+            .collection("Platforms")
+            .doc(Mail)
+            .collection("List")
+            .add({
+                Name: "live",
+                URL: URL
+            });
+
+        if (add.id) {
+            await db
+                .collection("Platforms")
+                .doc(Mail)
+                .collection("List")
+                .doc(ID)
+                .delete();
+            return res.send({
+                status: 1,
+                message: "Stream agregado",
+                reason: add.id
+            });
+        } else {
+            return res.send({
+                status: 0,
+                message: "Stream no pudo ser agregado",
+                rason: "gg"
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        return res.send({
+            status: 0,
+            message: "Stream no pudo ser agregado",
+            rason: err.message.toString()
+        });
+    }
+});
 
 exports.API = functions.https.onRequest(app);
