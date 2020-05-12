38b735f2b748bd31a63c4a4b3c7c07de08215aa8
amenpunk <ondasycircuitos@gmail.com>
2020-05-11 20:52:57 -0600

    [ADD] auth middleware

--- functions/Models/Users.js
+++ functions/Models/Users.js
@@ -1,24 +1,41 @@
 const { db, ErrorHandler } = require("../index");
+const admin = require("firebase-admin");
 const { Tools } = require("../Utils/tools");
+const JWT = require("jsonwebtoken");
+
+const SECRET = "ANIME-BITCHES";
 
 async function AddUser(req, res, next) {
     try {
-        const { Secret, Email, Token, Name } = req.body;
-        const requiredFields = Tools.Fields({ Secret, Email, Token, Name });
-        if(requiredFields){
-            throw new ErrorHandler(200, "Ingresa todos los campos")
+        const { Email, Name, UserID } = req.body;
+
+        const requiredFields = Tools.Fields({ Email, UserID, Name });
+
+        if (requiredFields) {
+            throw new ErrorHandler(200, "Ingresa todos los campos");
         }
-        const doc = await db.collection("Usuarios").add({
-            Secret, Email, Token, Name 
-        })
-        if(doc.id){
+        
+        const TOKEN = JWT.sign({
+            UserID,
+            Email,
+            Name
+        }, SECRET)
+
+        const doc = await db.collection("Usuarios").doc(UserID).set({
+            Email,
+            Name,
+            UserID,
+            TOKEN
+        });
+
+        if (doc.id) {
             return res.send({
-                status : 1,
-                title : "Exito!!",
-                message : "usuario creado exitosamente"
-            })
-        }else{
-            throw new ErrorHandler(400, "No pudimos crear al usuario")
+                status: 1,
+                title: "Exito!!",
+                message: "usuario creado exitosamente"
+            });
+        } else {
+            throw new ErrorHandler(400, "No pudimos crear al usuario");
         }
     } catch (err) {
         return next(err);
--- functions/controller.js
+++ functions/controller.js
@@ -1,12 +1,37 @@
 "use strict";
+const { ErrorHandler } = require("./index.js");
 
 const express = require("express");
 const StreamController = require("./Models/Stream");
 const UserController = require("./Models/Users");
+const JWT = require("jsonwebtoken");
+const SECRET = "ANIME-BITCHES";
 
 const api = express.Router();
 const user = express.Router();
 
+const AUTH = (req, res, next) => {
+    const header = req.headers["authorization"];
+    try {
+        const token = header.split(" ")[1];
+        return JWT.verify(token, SECRET, (err, user) => {
+            if (err) {
+                return res.status(401).send({
+                    status: 0,
+                    title: "auth problem",
+                    message: "No estas autorizado para ver este contenido"
+                });
+            }
+            req.user = user;
+            console.log(user);
+            return next();
+        });
+    } catch (err) {
+        throw new ErrorHandler(200, err.message);
+    }
+};
+
+api.use(AUTH);
 api.post("/Get/", StreamController.GetStream);
 api.delete("/Delete", StreamController.DeleteStream);
 api.post("/Add", StreamController.AddStream);
